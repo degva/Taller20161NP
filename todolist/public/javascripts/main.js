198b81d49a6edbde80d670b5d5274845b064f3ad
Diego Vargas <degva.29@gmail.com>
2016-06-09 02:30:49 -0500

    Finished

--- todolist/public/javascripts/main.js
+++ todolist/public/javascripts/main.js
@@ -2,7 +2,7 @@ angular.module('todo-app', [])
 
 .controller('mainController', function($scope, $http) {
 	$scope.formData = {};
-	$scope.todoData = {};
+	$scope.todoData = [];
 
 	// llamamos a todos los items
 	$http.get('/api/v1/items')
@@ -13,4 +13,40 @@ angular.module('todo-app', [])
 		.error(function(error) {
 			console.log("Error: " + error);
 		});
+
+	// Creamos variable dentro del scope
+	$scope.createTodo = function() {
+		$http.post('/api/v1/items', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};
+				$scope.todoData = data.data;
+				console.log(data);
+			})
+			.error(function(err) {
+				console.log("error: " + error);
+			});
+	};
+
+	// Ponemos el check al todo
+	$scope.actualizaTodo = function(todoId) {
+		$http.put('/api/v1/items/' + todoId)
+			.success(function(data) {
+				console.log("yey!");
+			})
+			.error(function(err) {
+				console.log("error: " + error);
+			});
+	};
+
+	// Borra todo
+	$scope.borraTodo = function(todoId) {
+		$http.delete('/api/v1/items/' + todoId)
+			.success(function(data) {
+				console.log("yey!");
+			})
+			.error(function(err) {
+				console.log("error: " + error);
+			});
+	};
+
 })
--- todolist/routes/todoeng.js
+++ todolist/routes/todoeng.js
@@ -5,7 +5,7 @@ var todoeng = {
 	createItem: function(req, res) {
 		// tomamos y verificamos que se estan enviando datos
 		var description = req.body.description;
-		if (description != '') {
+		if (description == '') {
 			res.status(204);
 			res.json({
 				"status": 204,
@@ -17,16 +17,34 @@ var todoeng = {
 		// creamos una conexcion con la base de datos
 		var db = pgp(pgCn);
 
-		var query = "INSERT INTO items (descripcion, fecha_creacion) values ($1, now()) returning item_id";
+		var query = "INSERT INTO items (descripcion, fecha_creacion) values ($1, now())";
 
-		db.one(query, [decription])
+		db.none(query, [description])
 			.then(function(data) {
-				res.status(200);
-				res.json({
-					"status": 200,
-					"message": "Insercion exitosa",
-					"data": data.item_id
-				});
+				// volvemos a retomar toooodas los inserts
+				var items = [];
+
+				db.many("SELECT item_id, descripcion, hecho from items")
+					.then(function(data){
+						data.forEach(function(item) {
+							items.push({"id": item.item_id, "description": item.descripcion, "done": item.hecho});
+						});
+						res.status(200);
+						res.json({
+							"status": 200,
+							"message": "Insercion exitosa",
+							"data": items
+						});
+					})
+					.catch(function(err) {
+						console.log("ERROR: ", err);
+						res.status(401);
+						res.json({
+							"status": 401,
+							"message": err
+						});
+					});
+						
 			})
 			.catch(function(err) {
 				console.log("ERROR: ", err);
@@ -64,7 +82,7 @@ var todoeng = {
 		var item_id = req.params.id;
 		var db = pgp(pgCn);
 
-		var query = "UPDATE items SET hecho = (select not(hecho) from items where item_id = $1) where item_id = $1";
+		var query = "UPDATE items SET (fecha_terminado, hecho) = (now(), (select not(hecho) from items where item_id = $1)) where item_id = $1";
 
 
 		db.none(query, item_id)
@@ -87,7 +105,7 @@ var todoeng = {
 		var item_id = req.params.id;
 		var db = pgp(pgCn);
 
-		var query = "DELETE items WHERE item_id = $1";
+		var query = "DELETE FROM items WHERE item_id = $1";
 
 
 		db.none(query, item_id)
